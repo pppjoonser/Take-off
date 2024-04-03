ceba88a9745ac80715803a2ce9c981e07653bc9c
박준서 <pppg12jintpt@gmail.com>
2024-04-03 23:13:19 +0900

    총알 상대속도 추가

--- Assets/03_Scripts/Bullet.cs
+++ Assets/03_Scripts/Bullet.cs
@@ -7,20 +7,23 @@ public class Bullet : MonoBehaviour
     GameObject playerfire;
     public float _fireSpeed;
     [SerializeField] private float _delayFuze;
+    Vector2 _playerspeed;
     private void Awake()
     {
         playerfire = GameObject.Find("Player");
+
         
     }
 
     private void Start()
     {
+        _playerspeed = playerfire.GetComponent<Rigidbody2D>().velocity;
     }
     void Update()
     {
         float z = transform.rotation.eulerAngles.z + 90;
         Vector2 direction = new Vector2((Mathf.Cos(z * Mathf.Deg2Rad)), (Mathf.Sin(z * Mathf.Deg2Rad)));
-        GetComponent<Rigidbody2D>().velocity = direction * _fireSpeed;
+        GetComponent<Rigidbody2D>().velocity = direction * _fireSpeed + _playerspeed;
 
         _delayFuze -= Time.deltaTime;
         if (_delayFuze < 0)
